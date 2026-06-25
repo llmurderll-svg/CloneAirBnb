bf2d08d44081ae512d8dc0b93fbf48679d1035e8
Gustavo La Madrid <59756173+llmurderll-svg@users.noreply.github.com>
2026-06-24 22:29:52 -0500

    First View For Rooms Data

--- AirBnbClone/Core/Explore/ExploreView.swift
+++ AirBnbClone/Core/Explore/ExploreView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ScrollView{
+                LazyVStack(spacing: 32){
+                    ForEach(0...5, id: \.self){ listItem in
+                        ListItemsView()
+                            .frame(height: 400)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 10)
+                            )
+                    }
+                }
+                .padding()
+            }
+        }
     }
 }
 
--- AirBnbClone/Core/Listings/ListItemsView.swift
+++ AirBnbClone/Core/Listings/ListItemsView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ListItemsView: View {
+    
+    let images = [
+        "imageReference-1",
+        "imageReference-2",
+        "imageReference-3",
+        "imageReference-4",
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:8){
+            TabView{
+                ForEach(images, id: \.self){ image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    Text("12 MI Way")
+                        .foregroundStyle(.gray)
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    HStack{
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                HStack(spacing: 2){
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
     }
 }
 
